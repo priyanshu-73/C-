767e25066859701c382fe9efd075f45f33f24682
priyanshu-73 <112119073+priyanshu-73@users.noreply.github.com>
2023-04-24 08:35:26 +0530

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"enter a & b"<<endl;
+    cin>>a>>b;
+    if(a>b){
+        cout<<"a is greater"<<endl;
+    }
+    else{
+        cout<<"b is greater"<<endl;
+    }
+}
